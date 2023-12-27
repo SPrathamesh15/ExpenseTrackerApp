91d49dbfa4ed4954ba2aeefa5bb899d30c497094
SPrathamesh15 <sprathamesh354@gmail.com>
2023-12-27 11:53:56 +0530

    Added sequelize transaction method

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const Expense = require('../models/index');
 const User = require('../models/signup')
+const sequelize = require('../util/database')
 exports.getAddExpense = (req, res, next) => {
   res.render('expense/add-expense', {
     pageTitle: 'Add Expense',
@@ -8,6 +9,7 @@ exports.getAddExpense = (req, res, next) => {
 };
 
 exports.postAddExpense = async (req, res, next) => {
+  const t = await sequelize.transaction();
   try {
     const expenseAmount = req.body.expenseAmount;
     const expenseDescription = req.body.expenseDescription;
@@ -20,17 +22,22 @@ exports.postAddExpense = async (req, res, next) => {
       expenseDescription: expenseDescription,
       category: category,
       userId: userId
-    });
+    },
+    { transaction: t }
+    );
 
     // Update totalExpense in UserDetails
-    const user = await User.findByPk(userId);
+    const user = await User.findByPk(userId, { transaction: t });
     console.log(user)
     user.totalExpense = user.totalExpense + parseInt(expenseAmount);
-    await user.save();
+    await user.save({ transaction: t });
+
+    await t.commit();
 
     res.status(201).json({ newExpenseDetails: expense });
     console.log('Expense added to server');
   } catch (err) {
+    await t.rollback();
     res.status(500).json({ error: err.message });
     console.error(err);
   }
@@ -48,24 +55,36 @@ exports.getAllExpenses = async (req, res, next) => {
 
 exports.deleteExpense = async (req, res, next) => {
   const expenseId = req.params.expenseId;
+  const t = await sequelize.transaction();
 
   try {
     // Find the expense to check if it belongs to the logged-in user
     const expense = await Expense.findOne({
-      where: { id: expenseId, userId: req.user.id }
+      where: { id: expenseId, userId: req.user.id },
+      transaction: t
     });
 
     if (!expense) {
+      await t.rollback();
       return res.status(404).json({ message: 'Expense not found or unauthorized' });
     }
 
+    const user = await User.findByPk(req.user.id, { transaction: t });
+    if (user) {
+      user.totalExpense = user.totalExpense - expense.expenseAmount;
+      await user.save({ transaction: t });
+    }
     // Only delete the expense if it belongs to the logged-in user
     const result = await Expense.destroy({
-      where: { id: expenseId }
+      where: { id: expenseId },
+      transaction: t
     });
+
+    await t.commit();
 
     res.status(200).json({ message: 'Expense deleted successfully', result });
   } catch (err) {
+    await t.rollback();
     res.status(500).json({ error: err });
   }
 };
