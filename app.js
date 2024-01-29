e46a31a29a9889f0dc0b49f07f26f38ad56b034f
SPrathamesh15 <sprathamesh354@gmail.com>
2024-01-29 15:40:49 +0530

    updated

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use('/report', reportsRoutes)
 app.use((req, res, next) => {
     res.setHeader(
         'Content-Security-Policy',
-        "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com 'unsafe-inline'; script-src 'self' https://checkout.razorpay.com 'unsafe-inline'; style-src 'self' https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/ 'unsafe-inline'; connect-src 'self' http://13.126.112.76:3000"
+        "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com https://checkout.razorpay.com 'unsafe-inline'; style-src 'self' https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/ 'unsafe-inline'; connect-src 'self' http://13.126.112.76:3000"
     );
     next();
 });
